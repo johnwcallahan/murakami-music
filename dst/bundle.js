3379c62add1f62f32ff0ef4ce1553ec7fa431509
johnwcallahan <johnwcallahan@gmail.com>
2018-01-21 17:54:42 -0800

    correct api endpoints

--- dst/bundle.js
+++ dst/bundle.js
@@ -19112,12 +19112,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } // import _ from "lodash";
-
-
-// import ComposerFilter from "./ComposerFilter";
-
-// import SpotifyPlayer from "./SpotifyPlayer";
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var BREAKPOINT_MD = 992;
 
@@ -38136,11 +38131,11 @@ var _axios2 = _interopRequireDefault(_axios);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var fetchParams = exports.fetchParams = function fetchParams(param) {
-  return _axios2.default.get("http://localhost:3000/api/" + param);
+  return _axios2.default.get("/api/" + param);
 };
 
 var fetchRefs = exports.fetchRefs = function fetchRefs(params) {
-  return _axios2.default.post("http://localhost:3000/api/refs", params);
+  return _axios2.default.post("/api/refs", params);
 };
 
 /***/ }),
--- src/util/requests.js
+++ src/util/requests.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const fetchParams = (param) => {
-  return axios.get("http://localhost:3000/api/" + param);
+  return axios.get("/api/" + param);
 };
 
 export const fetchRefs = (params) => {
-  return axios.post("http://localhost:3000/api/refs", params);
+  return axios.post("/api/refs", params);
 };

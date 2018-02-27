041656a0e393fa0feb3908763fc89e00f85cd9db
johnwcallahan <johnwcallahan@gmail.com>
2018-02-27 08:16:09 -0800

    reconfigure playlistmodal component as class, so own state can be used

--- src/components/PlaylistModal.js
+++ src/components/PlaylistModal.js
@@ -5,52 +5,72 @@ import { Button, Modal, Table } from "react-bootstrap";
 import getArtists from "../logic/getArtists";
 import getDuration from "../logic/getDuration";
 
-const PlaylistModal = (props) => {
-  
-  let trackInfo = props.currentPlaylist.map((track, i) => {
+class PlaylistModal extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  render() {
+    let trackInfo = this.props.currentPlaylist.map((track, i) => {
+      return (
+        <tr key={track.id+i}>
+          <td>{i+1}</td>
+          <td>{track.name}</td>
+          <td>{getArtists(track)}</td>
+          <td>{getDuration(track)}</td>
+        </tr>
+      );
+    });
+
     return (
-      <tr key={track.id+i}>
-        <td>{i+1}</td>
-        <td>{track.name}</td>
-        <td>{getArtists(track)}</td>
-        <td>{getDuration(track)}</td>
-      </tr>
+      <Modal show={this.props.show}>
+        <Modal.Header>
+          <Modal.Title>Create playlist</Modal.Title>
+          <input onChange={(e) => this.handleInputChange(e)}
+                type="text"
+                placeholder="Playlist name" 
+          />
+        </Modal.Header>
+    
+        <Modal.Body>
+          <Table hover condensed>
+            <thead>
+              <tr> 
+                <th>#</th>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Duration</th>
+              </tr>
+            </thead>
+            <tbody>
+              {trackInfo}
+            </tbody>
+          </Table>
+        </Modal.Body>
+    
+        <Modal.Footer>
+          <Button onClick={this.props.handleHide}>Close</Button>
+          <Button bsStyle="primary" 
+                  onClick={() => this.props.createPlaylist(this.state.input)}>Create</Button>
+        </Modal.Footer>
+      </Modal>
     );
-  });
-
-  return (
-    <Modal show={props.show}>
-      <Modal.Header>
-        <Modal.Title>Create playlist</Modal.Title>
-      </Modal.Header>
-
-      <Modal.Body>
-        <Table hover condensed>
-          <thead>
-            <tr> 
-              <th>#</th>
-              <th>Title</th>
-              <th>Artist</th>
-              <th>Duration</th>
-            </tr>
-          </thead>
-          <tbody>
-            {trackInfo}
-          </tbody>
-        </Table>
-      </Modal.Body>
-
-      <Modal.Footer>
-        <Button onClick={props.handleHide}>Close</Button>
-        <Button bsStyle="primary" onClick={() => props.getTrackInfo()}>Save changes</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-};
+  }
+}
 
 PlaylistModal.propTypes = {
   handleHide: PropTypes.func,
-  getTrackInfo: PropTypes.func,
+  createPlaylist: PropTypes.func,
   show: PropTypes.bool.isRequired,
   currentPlaylist: PropTypes.array
 };

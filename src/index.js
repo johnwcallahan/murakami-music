081dff3b44db66caac9eaeb3cc991abb6fda71fb
johnwcallahan <johnwcallahan@gmail.com>
2018-02-28 07:26:06 -0800

    save state splice to localstorage and use as default state

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-// React
 import React from "react";
 import { render } from "react-dom";
-
 import thunkMiddleware from "redux-thunk";
-
 import { Provider } from "react-redux";
-
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import { authMiddleware } from "redux-implicit-oauth2";
+import { loadState, saveState } from "./util/helpers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import throttle from "lodash/throttle";
+
+const persistedState = loadState();
+
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunkMiddleware,authMiddleware)
+  persistedState,
+  composeWithDevTools(
+    applyMiddleware(thunkMiddleware, authMiddleware)
+  )
 );
+
+store.subscribe(throttle(() => {
+  saveState({
+    spotifyUserId: store.getState().spotifyUserId
+  });
+}), 1000);
 
 import App from "./components/App";
 
--- src/util/helpers.js
+++ src/util/helpers.js
@@ -32,3 +32,23 @@ export const toggleSelected = (params, title) => {
   return params;
 };
 
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState == null)
+      return undefined;
+    return JSON.parse(serializedState);
+  } catch(err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  console.log(state);
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch(err) {
+    console.log(err);
+  }
+};

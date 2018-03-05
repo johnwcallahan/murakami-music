39ee0a89895d3dc46a3783045459226505f5d135
johnwcallahan <johnwcallahan@gmail.com>
2018-03-04 16:21:28 -0800

    changes to procfile

--- src/constants/oauthConfig.js
+++ src/constants/oauthConfig.js
@@ -1,7 +1,7 @@
 const config = {
   url: "https://accounts.spotify.com/authorize",
   client: "7ee5f70f185645139c74ac5efa7c3bbd",
-  redirect: "http://localhost:8080/callback",
+  redirect: "http://murakami-music.herokuapp.com/callback",
   scope: "user-library-read,playlist-modify-private,playlist-modify-public,user-modify-playback-state",
   width: 400, 
   height: 400

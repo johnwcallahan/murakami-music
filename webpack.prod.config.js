6f33bc90bd4965727fbb26a635f8bee35da4cf74
johnwcallahan <johnwcallahan@gmail.com>
2018-03-04 16:49:12 -0800

    removed eslint from webpack prod config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -19,12 +19,6 @@ module.exports = {
   module: {
     loaders: [
       {
-        enforce: "pre",
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
-      },
-      {
         test: /\.jsx?$/,
         loader: "babel-loader",
         exclude: /node_modules/
